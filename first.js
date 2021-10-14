687bc34e0185b20c6274b00cfdb81e8ec52ecc71
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-15 01:15:12 +0600

    added some varibale

--- first.js
+++ first.js
@@ -5,3 +5,10 @@ var tempareture = 38;
 console.log(age);
 console.log(tempareture);
 console.log(Eggprice);
+var nam = "sodur uddin"
+var address = "vuter goli nil kher";
+var gf = "aliya khaton"
+var school = "Narsingdi Polytechnic Institute"
+
+var pass = true;
+var subscribed = false;
