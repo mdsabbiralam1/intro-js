090ea91860b32d1533f97b6f6a663eacd559de5f
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-15 01:37:24 +0600

    added variable name js

---
+++ variable-name.js
@@ -0,0 +1,21 @@
+var name = 'hero alom'
+
+// Reservrd keyword not Work
+var class = 125; 
+var function = 'solimulla vai'
+
+// Name shoud be one worde 
+var Student Name = 'sabbir vai';  wrong
+var studentName = "sabbir";  right
+
+ // qoute is not allow
+ var 'nayka' = 'purnima';
+ var my-official-address = 'sundorbon branch'
+ var my_official_address = 'sundorbon branch'
+ var myOfficialAaddress = 'sundorbon branch'
+ var myofficialaddress = 'sundorbon branch'
+
+ var moa = 'nikonjo vobon'
+ var 99price = 898;
+ var price987 = 5754;
+ 
