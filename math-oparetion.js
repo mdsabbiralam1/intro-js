435d733928224fd86fe56b807f74371ed9e5d257
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-17 03:37:00 +0600

    math oparetion complete

--- math-oparetion.js
+++ math-oparetion.js
@@ -7,7 +7,27 @@ var difference = price2 - price1;
 var multiplication = price1 * price2;
 var division = price1 / price2;
 
-console.log(sum)
-console.log(difference)
-console.log(multiplication)
-console.log(division)
+// console.log(sum)
+// console.log(difference)
+// console.log(multiplication)
+// console.log(division)
+
+
+var newPrice = price1 + 10;
+console.log(newPrice);
+// or
+var price1 = price1 + 10;
+console.log(price1);
+// or
+price1 += 10;
+console.log(price1);
+
+
+var age = 18;
+// age += 1;
+console.log(age);
+age ++;
+
+var love = 100;
+ love -= 10;
+console.log(love);
