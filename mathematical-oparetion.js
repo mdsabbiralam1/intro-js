1e32d6bc75f787710d5ae340556a804a6ccc84ba
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-17 02:27:39 +0600

    added multiplicaton js

---
+++ mathematical-oparetion.js
@@ -0,0 +1,13 @@
+var onionPrice = 52;
+var eggPrice = 35;
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+// console.log(onionPrice+eggPrice);
+
+var priceDifference = onionPrice - eggPrice;
+// console.log(priceDifference);
+
+var penPrice = 5;
+var penQuantity = 15;
+var penPriceMultiplication = penPrice * penQuantity;
+console.log(penPriceMultiplication);
