b4447a5ec1650b5825718d09f799b444437a5623
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-18 02:28:49 +0600

    added string sum

---
+++ conversion.js
@@ -0,0 +1,9 @@
+var firstName = 'sabbir';
+var lastName = 'Alam';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+var firstName1 = 'Rakibul';
+var lastName1 = 'Hasan';
+var fullName1 = firstName1 + ' ' + lastName1;
+console.log(fullName1);
