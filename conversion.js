e73e5073887c2fda4971fed1ffee2b87563dffa8
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-18 02:51:31 +0600

    add parse part

--- conversion.js
+++ conversion.js
@@ -7,3 +7,21 @@ var firstName1 = 'Rakibul';
 var lastName1 = 'Hasan';
 var fullName1 = firstName1 + ' ' + lastName1;
 console.log(fullName1);
+
+
+var onionPrice = '54';
+var eggPrice = '40';
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice)
+
+
+// parse and float
+var onionPriceNumber = parseInt(onionPrice);
+var eggPriceNumber = parseInt(eggPrice);
+// console.log(eggPriceNumber);
+// console.log(onionPriceNumber);
+
+var totalNewPrice = onionPriceNumber + eggPriceNumber;
+console.log(totalNewPrice);
+
+
