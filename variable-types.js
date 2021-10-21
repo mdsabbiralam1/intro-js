b7e5f028d9cfa0f1f5ded891bce927c8550e52ff
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-21 22:23:02 +0600

    recap

--- variable-types.js
+++ variable-types.js
@@ -1,3 +1,29 @@
 var tshirtPrice = 200;
-console.log(typeof tshirtPrice);
-console.log(tshirtPrice);
+// console.log(typeof tshirtPrice);
+// console.log(tshirtPrice);
+
+var className = 'seven';
+// console.log(typeof className);
+// console.log(className);
+
+
+var subscribes = false;
+// console.log(typeof subscribes);
+// console.log(subscribes);
+
+
+
+// recap
+var rool = 58;
+var className = 'seven';
+var passed = false;
+var rima;
+// console.log(typeof rool);
+// console.log(typeof className);
+// console.log(typeof passed);
+// console.log(typeof rima);
+
+var today = new Date();
+console.log(today);
+var today = Date();
+console.log(today);
