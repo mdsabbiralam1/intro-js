4fa837cd3be6ec743c14d2d90d17ce3d8ab919f2
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-10-22 01:05:35 +0600

    add summary file

---
+++ summary.js
@@ -0,0 +1,26 @@
+var flowerQuantity = 23;
+flowerQuantity = 23 + 2;
+console.log(flowerQuantity);
+
+// string
+var foodName = 'Mango';
+console.log(typeof foodName);
+
+// number
+var fishQuantity = 556;
+console.log(typeof fishQuantity);
+
+// bollean 
+var isRain = true;
+console.log(typeof isRain);
+
+
+// mathmatical oparetion - modulus
+var mangoes = 7;
+var hungryPeople = 2;
+
+var divite = mangoes / hungryPeople;
+console.log(divite);
+
+var modulus = mangoes % hungryPeople;
+console.log(modulus);
